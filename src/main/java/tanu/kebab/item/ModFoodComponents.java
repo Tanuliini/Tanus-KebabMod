075fc5bfb292956e853276a2d8e198fd3ba16f43
Tanu <taneliin12345@gmail.com>
2025-10-04 17:20:16 +0300

    Everything Updated

--- src/main/java/tanu/kebab/item/ModFoodComponents.java
+++ src/main/java/tanu/kebab/item/ModFoodComponents.java
@@ -1,16 +1,35 @@
 package tanu.kebab.item;
 
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.ConsumableComponents;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 
 public class ModFoodComponents {
     public static final FoodComponent KEBAB = new FoodComponent.Builder().nutrition(6).saturationModifier(0.65f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.SATURATION, 200), 0.05f).build();
+            .build();
 
     public static final FoodComponent RAW_KEBAB = new FoodComponent.Builder().nutrition(2).saturationModifier(0.2f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 300), 0.35f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 300), 0.05f)
+            .build();
+    public static final ConsumableComponent RAW_KEBAB_EFFECT = ConsumableComponents.food()
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.HUNGER, 300), 0.35f))
+            .build();
+
+    public static final FoodComponent KEBAB_BREAD = new FoodComponent.Builder().nutrition(6).saturationModifier(1.2F)
+            .build();
+
+    public static final FoodComponent KEBAB_LEIPA_KASTIKKEELLA = new FoodComponent.Builder().nutrition(6).saturationModifier(0.65f)
+            .build();
+    public static final ConsumableComponent KEBAB_LEIPA_KASTIKKEELLA_EFFECT = ConsumableComponents.food()
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.SATURATION, 800), 1f))
+            .build();
+
+    public static final FoodComponent OVER_COOKED_KEBAB_BREAD = new FoodComponent.Builder().nutrition(2).saturationModifier(0.3F)
+            .build();
+    public static final ConsumableComponent OVER_COOKED_KEBAB_BREAD_EFFECT = ConsumableComponents.food()
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.HUNGER, 150), 1f))
             .build();
 
 }
--- src/main/java/tanu/kebab/item/ModItems.java
+++ src/main/java/tanu/kebab/item/ModItems.java
@@ -1,20 +1,42 @@
 package tanu.kebab.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import tanu.kebab.TanusKebabMod;
 
-public class ModItems {
-    public static final Item KEBAB = registerItem("kebab", new Item(new Item.Settings().food(ModFoodComponents.KEBAB)));
-    public static final Item RAW_KEBAB = registerItem("raw_kebab", new Item(new Item.Settings().food(ModFoodComponents.RAW_KEBAB)));
-    public static final Item KEBAB_BREAD = registerItem("kebab_bread", new Item(new Item.Settings()));
+import java.util.function.Function;
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(TanusKebabMod.MOD_ID, name), item);
+public class ModItems {
+    public static final Item KEBAB_KASTIKE = registerItem("kebab_kastike", Item::new);
+
+
+    public static final Item KEBAB = registerItem("kebab", setting -> new Item(setting
+            .food(ModFoodComponents.KEBAB)));
+    public static final Item RAW_KEBAB = registerItem("raw_kebab", setting -> new Item(setting
+            .food(ModFoodComponents.RAW_KEBAB, ModFoodComponents.RAW_KEBAB_EFFECT)));
+
+
+    public static final Item KEBAB_BREAD = registerItem("kebab_bread", setting -> new Item(setting
+            .food(ModFoodComponents.KEBAB_BREAD)));
+
+    public static final Item OVER_COOKED_KEBAB_BREAD = registerItem("over_cooked_kebab_bread", setting -> new Item(setting
+            .food(ModFoodComponents.OVER_COOKED_KEBAB_BREAD, ModFoodComponents.OVER_COOKED_KEBAB_BREAD_EFFECT)));
+
+    public static final Item KEBAB_LEIPA_KASTIKKEELLA = registerItem("kebab_leipa_kastikkeella", setting -> new Item(setting
+            .food(ModFoodComponents.KEBAB_LEIPA_KASTIKKEELLA, ModFoodComponents.KEBAB_LEIPA_KASTIKKEELLA_EFFECT)));
+
+
+
+    private static Item registerItem(String name, Function<Item.Settings, Item> function) {
+        return Registry.register(Registries.ITEM, Identifier.of(TanusKebabMod.MOD_ID, name),
+                function.apply(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TanusKebabMod.MOD_ID, name)))));
     }
 
     public static void registerModItems() {
@@ -30,6 +52,15 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(KEBAB_BREAD);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(OVER_COOKED_KEBAB_BREAD);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(KEBAB_LEIPA_KASTIKKEELLA);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(KEBAB_KASTIKE);
         });
     }
 }
